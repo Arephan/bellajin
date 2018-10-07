d47115c015b7196153d4cedd472de00bec782cda
Han Kim <hankimproduction@gmail.com>
2018-10-06 22:34:56 -0400

    Implement Bellajin headerLinks

--- src/views/Components/Components.jsx
+++ src/views/Components/Components.jsx
@@ -36,7 +36,7 @@ class Components extends React.Component {
     return (
       <div>
         <Header
-          brand="Material Kit React"
+          brand="BellaJin Hair"
           rightLinks={<HeaderLinks />}
           fixed
           color="transparent"
