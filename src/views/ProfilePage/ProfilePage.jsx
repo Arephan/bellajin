4b1cf00d274b644cf0e96338a03b90cf9a556e09
Han Kim <hankimproduction@gmail.com>
2018-10-31 14:03:58 -0400

    Finish profile photo implementation

--- src/views/ProfilePage/ProfilePage.jsx
+++ src/views/ProfilePage/ProfilePage.jsx
@@ -11,7 +11,9 @@ import Parallax from "components/Parallax/Parallax.jsx";
 import SimpleTable from "components/SimpleTable/SimpleTable";
 import { firebaseAuth } from "firebase/constants";
 import { getUserAppointments } from "firebase/db";
+import FileUploader from "react-firebase-file-uploader";
 import React from "react";
+import firebase from "firebase";
 
 class ProfilePage extends React.Component {
   constructor(props) {
@@ -19,9 +21,40 @@ class ProfilePage extends React.Component {
     this.state = {
       currentUser: firebaseAuth().currentUser,
       userAppointments: [],
-      loading: true
+      loading: true,
+      isUploading: false,
+      progress: 0,
+      photoURL: firebaseAuth().currentUser.photoURL || profile
     };
   }
+
+  handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
+  handleProgress = progress => this.setState({ progress });
+  handleUploadError = error => {
+    this.setState({ isUploading: false });
+    console.error(error);
+  };
+  handleUploadSuccess = filename => {
+    this.setState({ avatar: filename, progress: 100, isUploading: false });
+    firebase
+      .storage()
+      .ref("images/" + this.state.currentUser.uid)
+      .child(filename)
+      .getDownloadURL()
+      .then(url => {
+        this.setState({ photoURL: url });
+        this.state.currentUser
+          .updateProfile({
+            photoURL: url
+          })
+          .then(function() {
+            // Update successful.
+          })
+          .catch(function(error) {
+            // An error happened.
+          });
+      });
+  };
 
   async getAppointments() {
     const resp = await getUserAppointments(this.state.currentUser.uid);
@@ -48,11 +81,29 @@ class ProfilePage extends React.Component {
         <div className={classNames(classes.main, classes.mainRaised)}>
           <div>
             <div className={classes.container}>
+              {this.state.isUploading && (
+                <CustomLinearProgress color="info" variant="indeterminate" />
+              )}
               <GridContainer justify="center">
                 <GridItem md={6} sm={12} xs={12}>
                   <div className={classes.profile}>
                     <div>
-                      <img src={profile} alt="..." className={imageClasses} />
+                      <label>
+                        <FileUploader
+                          hidden
+                          accept="image/*"
+                          storageRef={firebase.storage().ref("images")}
+                          onUploadStart={this.handleUploadStart}
+                          onUploadError={this.handleUploadError}
+                          onUploadSuccess={this.handleUploadSuccess}
+                          onProgress={this.handleProgress}
+                        />
+                        <img
+                          src={this.state.photoURL}
+                          alt="..."
+                          className={imageClasses}
+                        />
+                      </label>
                     </div>
                     <div className={classes.name}>
                       <h3 className={classes.title}>
@@ -69,7 +120,6 @@ class ProfilePage extends React.Component {
                       <SimpleTable data={this.state.userAppointments} />
                     )}
                   </div>
-                  <div id="messages" />
                 </GridItem>
               </GridContainer>
             </div>
